52392b6f2d8ea323132c41950b0273790817ab16
im4i4a <al.zemskov03@yandex.ru>
2025-09-24 00:27:53 +0300

    master: Create project for module 3

--- PracForCsharp/Program.cs
+++ PracForCsharp/Program.cs
@@ -1,14 +1,20 @@
 ﻿
 
+using System;
+
 namespace PracForCsharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Aleksey";
+            const string MyName = "Aleksey";
             Console.WriteLine(MyName);
+
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine("\tМне 22 года");
+            Console.WriteLine("\nMy name is {0}", MyName);
+            Console.WriteLine("\x03");
         }
     }
 }
